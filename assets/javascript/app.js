4dbb488fc8b5d8c2ea842aa1f6dcc1e8f03c926e
Lance Martin <ldmartin7777@gmail.com>
2016-06-05 18:33:54 -0500

    fully works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,21 +10,12 @@ $('#addTrain').on('click', function(event) {
   var firstTime =$('#startInput').val().trim();
   console.log($('#frequency').val());
   var frequency =$('#frequency').val().trim();
-  var firstTimeConverted = moment(firstTime,"hh:mm").subtract(1, "years");
-  var currentTime = moment();
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % frequency;
-  var tMinutesTillTrain = frequency - tRemainder;
-  // Next Train
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  var nextTime = moment(nextTrain).format("hh:mm");
 
   var train = {
     name: trainName,
     destination: trainDestination,
     often: frequency,
-    nextArrival: nextTime,
-    minutesTill: tMinutesTillTrain,
+    first: firstTime,
   };
 
   console.log(train);
@@ -45,11 +36,17 @@ myBase.on("child_added", function(childSnapshot, prevChildKey){
 	var tName = childSnapshot.val().name;
 	var tDestination = childSnapshot.val().destination;
 	var tFrequency = childSnapshot.val().often;
-	var tArrival = childSnapshot.val().nextArrival;
-  var tTill = childSnapshot.val().minutesTill;
+	var tFirst = childSnapshot.val().first;
+  var firstTimeConverted = moment(tFirst,"hh:mm").subtract(1, "years");
+  //var currentTime = moment();
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var tRemainder = diffTime % tFrequency;
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  var nextTime = moment(nextTrain).format("hh:mm");
 
 	// Add each train's data into the table
-	$("tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tTill + "</td></tr>");
+	$("tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + nextTime + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 
 });
 
